f401f475ec0ceffec5984b9cdba0e34ccd944afc
Aimen-ben <131805338+Aimen-ben@users.noreply.github.com>
2023-07-27 10:14:47 +0200

    Add files via upload

---
+++ accueil.php
@@ -0,0 +1,22 @@
+<?php
+
+// Instanciation de la classe FilmDAO
+$filmDAO = new FilmDAO();
+
+// Récupération de tous les films depuis la base de données
+$films = $filmDAO->getAll();
+// var_dump($films);
+$resultats = $filmDAO->join();
+
+
+// Regrouper les films par leur identifiant (idFilm) pour éliminer les doublons
+$filmsGroupes = array();
+foreach ($films as $film) {
+    $idFilm = $film->getId();
+    if (!array_key_exists($idFilm, $filmsGroupes)) {
+        $filmsGroupes[$idFilm] = $film;
+    }
+}
+
+echo $twig->render('accueil.html.twig', ['films' => $filmsGroupes, 'resultats' => $resultats]);
+
